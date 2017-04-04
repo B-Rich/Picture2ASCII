2681d72b5acf48af52c7a4a6c57847d014eeee5f
Lewis Kim (Sunghyun Kim) <kshr2d2@gmail.com>
2017-04-04 15:16:01 +0900

    Bitmap 2 Ascii v0.1

--- Bitmap2Ascii.py
+++ Bitmap2Ascii.py
@@ -1,29 +1,50 @@
-import numpy
 from PIL import Image as im
-import numpy
 
 class bitmap2ascii:
-    def convert2gray(filename):
+    def convert(filename):
+
         try:
             file = im.open(filename)
-            file.convert("L", colors= 8).save("gray" + filename)
+            file.convert("L", colors= 16).save("gray_" + filename)
+
         except:
             return "Not image or No such file"
-    def convert2ascii(filename):
-        pix = im.open(filename)
+
+        pix = im.open("gray_" + filename)
         w, h = pix.size
-        i, j = 0
-        while i == h:
-            while j == w:
-                pixdata = pix.getpixel(i, j)
 
+        with open('Ascii_'+ filename + '.txt', 'w') as file:
+            asciiList = ['@', '#', '=', '+', '-', '.', ' ']
 
+            for i in range(h):
+                for j in range(w):
+                    loc = (j, i)
+                    pixdata = pix.getpixel(loc)
+                    if pixdata in range(11, 43):
+                        asciipix = asciiList[0]
+                        file.write(asciipix)
+                    elif pixdata in range(43, 88):
+                        asciipix = asciiList[1]
+                        file.write(asciipix)
+                    elif pixdata in range(88, 137):
+                        asciipix = asciiList[2]
+                        file.write(asciipix)
+                    elif pixdata in range(137, 169):
+                        asciipix = asciiList[3]
+                        file.write(asciipix)
+                    elif pixdata in range(169, 190):
+                        asciipix = asciiList[4]
+                        file.write(asciipix)
+                    elif pixdata in range(190, 255):
+                        asciipix = asciiList[5]
+                        file.write(asciipix)
+                    else:
+                        asciipix = asciiList[6]
+                        file.write(asciipix)
 
+                        # lineMatrix.append(pixdata)
 
-
-test = bitmap2ascii.convert2ascii("포맷변환_test.bmp")
-print(test)
-
+                file.write("\n")
 
 
 
